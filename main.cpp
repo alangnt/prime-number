71d4467229cb4b10d486eb91403468c02ed0e33c
Alan Geirnaert <geirnaert.alan@icloud.com>
2026-09-23 21:51:48 +0200

    feat: check if prime number and build cmake

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
-bool is_prime(int number) {
-  // we're gonna define it here
+#include <iostream>
 
-  return false;
+bool is_prime_number(int number) {
+  int factors = 0;
+
+  if (number == 1) {
+    return true;
+  }
+
+  // we're gonna define it here
+  for (int i = 1; i < number; i++) {
+    if (number % i == 0) {
+      factors++;
+    }
+  }
+
+  return factors == 2;
 }
 
 int main() {
-  // let's define if a number is prime
+  for (int i = 1; i < 100; i++) {
+    bool is_prime = is_prime_number(i);
+
+    if (is_prime) {
+      std::cout << i << " is Prime." << std::endl;
+    } else {
+      std::cout << i << " is not Prime." << std::endl;
+    }
+  }
+
   return 0;
 }
