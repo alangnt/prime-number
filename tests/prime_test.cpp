b023cd477cbc9ed2488fa2a3cfa0ece0942d0d45
Alan Geirnaert <geirnaert.alan@icloud.com>
2026-09-24 15:45:27 +0200

    feat: updated tests

--- tests/prime_test.cpp
+++ tests/prime_test.cpp
@@ -1,18 +1,30 @@
 #include "prime.h"
 #include <gtest/gtest.h>
 
-TEST(PrimeTest, TwoIsPrime) {
+TEST(PrimeTest, IsPrime) {
   EXPECT_TRUE(is_prime_number(2));
+  EXPECT_TRUE(is_prime_number(3));
+  EXPECT_TRUE(is_prime_number(5));
+  EXPECT_TRUE(is_prime_number(7));
+  EXPECT_TRUE(is_prime_number(13));
 }
 
-TEST(PrimeTest, FourIsNotPrime) {
+TEST(PrimeTest, IsNotPrime) {
   EXPECT_FALSE(is_prime_number(4));
-}
-
-TEST(PrimeTest, SeventeenIsPrime) {
-  EXPECT_TRUE(is_prime_number(17));
+  EXPECT_FALSE(is_prime_number(6));
+  EXPECT_FALSE(is_prime_number(9));
+  EXPECT_FALSE(is_prime_number(15));
+  EXPECT_FALSE(is_prime_number(20));
 }
 
 TEST(PrimeTest, NegativeNumberIsNotPrime) {
-  EXPECT_FALSE(is_prime_number(-2));
+  EXPECT_FALSE(is_prime_number(-1));
+  EXPECT_FALSE(is_prime_number(-3));
+  EXPECT_FALSE(is_prime_number(-5));
+  EXPECT_FALSE(is_prime_number(-7));
+  EXPECT_FALSE(is_prime_number(-13));
+}
+
+TEST(PrimeTest, ZeroIsNotPrime) {
+  EXPECT_FALSE(is_prime_number(0));
 }
